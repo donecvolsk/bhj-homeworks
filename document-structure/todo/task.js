bdbd92ca28f91f0af37dd57af3ab39ab94f40cb5
donecvolsk <donecvolsk@yandex.ru>
2023-12-14 14:05:13 +0400

    выполнено задание todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task__input');
+
+const tasksList = document.getElementById('tasks__list');
+const task = document.querySelector('.task');
+const taskTitle = document.querySelector('.task__title');
+
+form.addEventListener('submit', (event) => { 
+    event.preventDefault();
+    
+    let newDiv = document.createElement('div');
+    newDiv = task.cloneNode(true);
+    taskTitle.textContent = taskInput.value;
+    tasksList.appendChild(newDiv);
+
+    event.target.reset();
+})
+
+
+
+
+
+
+    
+
+
+
+
