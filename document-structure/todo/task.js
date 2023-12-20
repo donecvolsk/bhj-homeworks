4089eb398aeaeb5f8579a01a59c619812f2cb2ac
donecvolsk <donecvolsk@yandex.ru>
2023-12-20 12:03:55 +0400

    выполнено задание доработка2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,45 +1,31 @@
 
 const form = document.querySelector('form');
 let taskInput = document.getElementById('task__input');
+//const tasksAdd = document.getElementById('tasks__add');
 const tasksList = document.querySelector('.tasks__list');
 
 
-/*let task = function() {tasksList.innerHTML +=
-`<div class="task">
-<div class="task__title">
-${taskInput.value}
-</div>
-<a href="#" class="task__remove">&times;</a>
-</div>`; }*/
-
-//доработка, прменен метод insertAdjacentHTML
-let task = function() {
-	tasksList.insertAdjacentHTML('beforebegin', 
-	`<div class="task">
-<div class="task__title">
-${taskInput.value}
-</div>
-<a href="#" class="task__remove">&times;</a>
-</div>`
-)
-}
-
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	//доработка, проверка условия на пустую строку или строку с пробелами
 	if (taskInput.value.trim() != '') {
-		task();
+		tasksList.insertAdjacentHTML('beforebegin', 
+		`<div class="task">
+		<div class="task__title">
+		${taskInput.value}
+		</div>
+		<a href="#" class="task__remove" onclick = "remove(this)">&times;</a>
+		</div>`
+		)
 		taskInput.value = "";
 	}
-	
-	let taskRemove = Array.from(document.querySelectorAll('.task__remove'));
-	for (let i = 0; i < taskRemove.length; i++) {
-	taskRemove[i].addEventListener('click', (event) => {
-		event.preventDefault();
-		event.target.closest(".task").remove();
-	})	
-}
 })
+
+function remove(el) {
+	let element = el;
+  	element.closest('.task').remove();
+}
+
+
 
 
 
